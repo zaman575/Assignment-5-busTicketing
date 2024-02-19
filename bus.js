431bcb348b12f0f53f1366962f91cd24a36d8adf
zaman575 <zamanbd20@gmail.com>
2024-02-19 20:13:26 +0600

    js start

--- bus.js
+++ bus.js
@@ -1,1 +1,21 @@
-console.log('hello');
+// document.getElementById("btn-buy-ticket").addEventListener("click", function (event) {
+//     console.log(event.target);
+    
+// });
+
+// function handleClick(event) {
+//     console.log(event.target.parentNode.parentNode.childNodes[1].innerText);
+// }
+
+const allBtn = document.getElementsByClassName("kbd");
+let setLeft = 40;
+
+for (const btn of allBtn) {
+    btn.addEventListener("click", function (event) {
+        setLeft = setLeft - 1;
+        document.getElementById("seats-left").innerText = setLeft;
+    })
+
+    
+}
+
